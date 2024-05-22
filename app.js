05fdf4a14b4bb363827eb6ecce1f38f1e3c78647
Teofilo Copa <teox777@gmail.com>
2024-05-22 08:20:10 -0400

    funcionalidad completa2

--- app.js
+++ app.js
@@ -7,6 +7,29 @@ app.set("llaveprivadajwt","mi clave ultrasecreta.123");
 app.use(bodyParser.urlencoded({ extended: true }));
 app.use(bodyParser.json());
 
+
+
+const rutasProtegidas = express.Router(); 
+rutasProtegidas.use((req, res, next) => {
+    const token = req.headers['token-de-acceso'];
+
+    if (token) {
+      jwt.verify(token, app.get('llaveprivadajwt'), (err, decoded) => {      
+        if (err) {
+          return res.json({ mensaje: 'Token no valido' });    
+        } else {
+          req.decoded = decoded;    
+          next();
+        }
+      });
+    } else {
+      res.send({ 
+          mensaje: 'Token no proveído.' 
+      });
+    }
+ });
+
+
 app.get('/', function(req, res) {
     res.send('Inicio');
 });
@@ -15,7 +38,7 @@ app.get('/public',(req, res)=>{
     res.send("Ruta publica");
 });
 
-app.get('/productos',(req, res)=>{
+app.get('/productos',rutasProtegidas,(req, res)=>{
     const productos = [
       { id: 1, nombre: "Asfo" },
       { id: 2, nombre: "Denisse" },
@@ -25,8 +48,23 @@ app.get('/productos',(req, res)=>{
      res.json(productos);
 });
 
-app.get('/login',(req, res)=>{
-    res.send("Formulario de login");
+app.post('/login',(req, res)=>{
+    //res.send("Formulario de login");
+
+    if(req.body.usuario === "admin" && req.body.clave === "admin") {
+        const payload = {
+        check:  true
+       };
+      const tokenGenerado = jwt.sign(payload, app.get('llaveprivadajwt'), {
+        expiresIn: 1440
+      });
+      res.json({
+        mensaje: 'Autenticación correcta',
+        token: tokenGenerado
+      });
+      } else {
+          res.json({ mensaje: "Usuario o contraseña incorrectos"})
+      }
 });
 
 // 5
